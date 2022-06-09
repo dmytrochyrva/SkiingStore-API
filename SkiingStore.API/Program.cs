c0727ddb47affed3fd07a1320d684a360721633e
Dmytro <dmytro.chyrva@protonmail.com>
2022-06-09 08:54:20 +0200

    [00001] MySql Database Added To The Project.

--- SkiingStore.API/Program.cs
+++ SkiingStore.API/Program.cs
@@ -2,13 +2,27 @@
 // Libraries Namespaces
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+// Local Namespaces
+using SkiingStore.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Configure MySql
+string connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+
+builder.Services.AddDbContext<SkiingStoreDb>(options =>
+{
+  options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
